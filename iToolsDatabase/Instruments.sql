5b702d85a6a118465ad6575336897840f445f665
Ravi Kiran <raviraaga1992@gmail.com>
2019-09-10 11:39:29 +0530

    Added Instruments table

---
+++ iToolsDatabase/Instruments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Instruments]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NTEXT NULL, 
+    [AssetId] INT NULL, 
+    [Enabled] BIT NULL, 
+    [SourceId] VARCHAR(50) NULL, 
+    [LastModified] DATETIME NULL
+)
