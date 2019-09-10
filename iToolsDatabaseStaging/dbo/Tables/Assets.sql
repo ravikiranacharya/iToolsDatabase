b5c25cded79a10b30f6e6a066e984c5eb44bbb58
Ravi Kiran <raviraaga1992@gmail.com>
2019-09-10 15:29:56 +0530

    Added LastModified column in Assets table in Staging

--- iToolsDatabaseStaging/dbo/Tables/Assets.sql
+++ iToolsDatabaseStaging/dbo/Tables/Assets.sql
@@ -2,6 +2,7 @@
     [Id]          INT   IDENTITY (1, 1) NOT NULL,
     [Description] NTEXT NULL,
     [Enabled]     BIT   NULL,
+    [LastModified] DATETIME NULL, 
     CONSTRAINT [PK_Assets] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
