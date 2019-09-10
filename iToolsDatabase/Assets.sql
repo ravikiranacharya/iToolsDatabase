410d004876dacdd1c5076625bd82c5ba1c70dbba
Ravi Kiran <raviraaga1992@gmail.com>
2019-09-10 11:33:36 +0530

    Added Assets table

---
+++ iToolsDatabase/Assets.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Assets]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Description] VARCHAR(50) NULL, 
+    [Enabled] BIT NULL, 
+    [SourceId] VARCHAR(50) NULL, 
+    [LastModified] DATETIME NULL
+)
